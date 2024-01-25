f2b579d2f0eadbdce3bf4e0aa50cd4f5b829d10e
donbignose <stamerra17@gmail.com>
2024-01-25 19:06:32 +0100

    feat: add nvim commands

--- lua/flowtime/init.lua
+++ lua/flowtime/init.lua
@@ -1,2 +1,10 @@
 local flowtime = require('flowtime.flowtime')
+vim.api.nvim_create_user_command(
+  'FlowTimeStart',
+  flowtime.start_flowtime,
+  { desc = 'Start Flowtime' }
+)
+vim.api.nvim_create_user_command('FlowTimeStop', flowtime.stop_flowtime, {})
+vim.api.nvim_create_user_command('FlowTimeStartBreak', flowtime.start_break, {})
+
 return flowtime
